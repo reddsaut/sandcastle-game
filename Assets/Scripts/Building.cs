0a2652f6c6e2558261fdc2e3fc7de98b6eb2a4fb
reddsaut <osauter@macalester.edu>
2024-09-30 17:17:12 +0200

    grid lock and prefab work

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,9 +1,12 @@
+using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Building : MonoBehaviour
 {    
     private bool placement;
+    public GameObject housePrefab;
     void Start()
     {
         placement = true;
@@ -16,7 +19,12 @@ public class Building : MonoBehaviour
             if(Input.GetMouseButtonDown(0))
             {
                 placement = false;
+                transform.position = new Vector3(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y) - 0.27f, 0);
             }
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            Instantiate(housePrefab, transform.position, Quaternion.identity);
         }
         
     }
